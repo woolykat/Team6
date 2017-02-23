3c0aaee28af1675f07d56ffec0cd9b30469fc00f
cleapatra <clea_allen@yahoo.com>
2017-02-23 14:36:36 +0000

    Add files via upload
    base.php connects to the database. For any page which you are using SQL/need connected to the database please use:
    <?php include "base.php"; ?>

---
+++ base.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+
+$conn = mysqli_connect("localhost", "Team6", "te8AJUJZ")
+    or die ("couldn't connect! Error:" . mysqli_error($conn));
+print "Successful Connection";
+mysqli_select_db($conn , 'Team6') or die ('Database will not open');
+
+
+
+
+
+
+
+
+mysqli_close ($conn);
+?>
